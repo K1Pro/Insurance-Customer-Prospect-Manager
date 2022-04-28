3bf767a80566a0f67b0500148fa2146ef914b71e
K1Pro <support@k1pro.net>
2022-04-28 08:39:58 +0200

    limiting search results to 10

--- search.js
+++ search.js
@@ -1,6 +1,7 @@
 const ContactsURL = 'http://192.168.54.22:4000/contacts';
 const message = document.getElementById('message');
 const firstNameSearch = document.getElementById('firstNameSearch');
+let rep = 0;
 
 //////////  Connecting to Database and Retrieving Data
 const getJSON = function (url, errorMsg = 'Something went wrong') {
@@ -11,29 +12,38 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 };
 
 const showSearchList = function (JsonDB) {
-  message.innerText = '';
-  console.log(firstNameSearch.value.toLowerCase());
+  console.log(`test 1: ${firstNameSearch.value.toLowerCase()}`);
+
+  if (rep < 10) {
+    message.innerText = '';
+  }
+
   for (let userData in JsonDB) {
-    const searchingFirstName = JsonDB[
-      userData
-    ].FirstName.toLowerCase().includes(firstNameSearch.value.toLowerCase());
+    if (rep < 10) {
+      const searchingFirstName = JsonDB[
+        userData
+      ].FirstName.toLowerCase().includes(firstNameSearch.value.toLowerCase());
 
-    const searchingLastName = JsonDB[userData].LastName.toLowerCase().includes(
-      firstNameSearch.value.toLowerCase()
-    );
+      const searchingLastName = JsonDB[
+        userData
+      ].LastName.toLowerCase().includes(firstNameSearch.value.toLowerCase());
 
-    const searchingSpouseName = JsonDB[userData].SpouseName.includes(
-      firstNameSearch.value
-    );
+      const searchingSpouseName = JsonDB[userData].SpouseName.includes(
+        firstNameSearch.value
+      );
 
-    if (searchingFirstName || searchingLastName || searchingSpouseName) {
-      message.innerText +=
-        JsonDB[userData].FirstName +
-        ' ' +
-        JsonDB[userData].LastName +
-        ' - ' +
-        JsonDB[userData].id +
-        '\n';
+      if (searchingFirstName || searchingLastName || searchingSpouseName) {
+        rep++;
+
+        console.log(`test 2: ${rep}`);
+        message.innerText +=
+          JsonDB[userData].FirstName +
+          ' ' +
+          JsonDB[userData].LastName +
+          ' - ' +
+          JsonDB[userData].id +
+          '\n';
+      }
     }
   }
 };
@@ -47,11 +57,11 @@ firstNameSearch.addEventListener('keyup', function (e) {
     e.key !== 'Control' &&
     e.key !== 'Alt'
   ) {
-    if (firstNameSearch.value.length > 3) {
-      // console.log(e.key);
-      getJSON(ContactsURL).then((data) => {
-        showSearchList(data);
-      });
-    }
+    // if (firstNameSearch.value.length > 3) {
+    // console.log(e.key);
+    getJSON(ContactsURL).then((data) => {
+      showSearchList(data);
+    });
+    // }
   }
 });
