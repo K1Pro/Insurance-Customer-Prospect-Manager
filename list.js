9ee9b03400174b9979047a09a7c1802cfcbfcd6b
Bartosz Kwasniewski <support@k1pro.net>
2022-06-01 20:42:20 +0200

    filtering lists functionality

--- list.js
+++ list.js
@@ -4,7 +4,8 @@ const contactList = document.getElementById('contactList');
 // prettier-ignore
 const contactListHeaders = document.getElementById('contactListHeaders').querySelectorAll('*');
 // prettier-ignore
-let tr, th, td, tdFirstName, tdLastName, tdAddress, tdAddress2, tdCity, tdState, tdZip, tdPhone, tdStatus, tdSource, tdLastEditDate, comparison;
+let tr, th, td, tdFirstName, tdLastName, tdAddress, tdAddress2, tdCity, tdState, tdZip, tdPhone, tdStatus, tdSource, tdLastEditDate, comparison, inputFirstName,  tdInputFirstName, inputLastName, tdInputLastName, inputAddress, tdInputAddress, inputAddress2, tdInputAddress2, inputCity, tdInputCity, inputState, tdInputState, inputZip, tdInputZip, inputPhone, tdInputPhone, inputStatus, tdInputStatus, inputSource, tdInputSource, inputLastEditDate, tdInputLastEditDate, filteredFirstName;
+let alternate = 0;
 // Example: FirstName = document.getElementById('FirstName').addEventListener('change, function(e){...});
 for (let rep = 0; rep < contactListHeaders.length; rep++) {
   let contactListHeadersIDs = contactListHeaders[rep].id;
@@ -17,11 +18,19 @@ for (let rep = 0; rep < contactListHeaders.length; rep++) {
         comparison = this.id;
         getJSON(ContactsURL).then((data) => {
           console.log(contactListHeadersIDs);
+          alternate++;
 
-          data.sort((a, b) =>
-            a[contactListHeadersIDs]?.localeCompare(b[contactListHeadersIDs])
-          );
+          if (alternate % 2) {
+            data.sort((a, b) =>
+              a[contactListHeadersIDs]?.localeCompare(b[contactListHeadersIDs])
+            );
+          } else {
+            data.sort((a, b) =>
+              b[contactListHeadersIDs]?.localeCompare(a[contactListHeadersIDs])
+            );
+          }
           console.log(data);
+          filterInputs();
           data.forEach((contactInfo) => {
             populateTable(contactInfo);
           });
@@ -39,6 +48,184 @@ async function getJSON(url, errorMsg = 'Something went wrong') {
   } catch (error) {
     console.log(errorMsg);
   }
+}
+
+function search(JsonDB) {
+  // if (
+  //   // e.key !== 'Backspace' &&
+  //   e.key !== 'Shift' &&
+  //   e.key !== 'CapsLock' &&
+  //   e.key !== 'Control' &&
+  //   e.key !== 'Alt'
+  // ) {
+  // console.log(inputFirstName.value);
+  let test = inputFirstName.value;
+  contactList.innerHTML = '';
+  let filteredData = JsonDB.filter((userData) => {
+    if (
+      userData.FirstName?.toLowerCase().slice(0, inputFirstName.value.length) ==
+      inputFirstName.value.toLowerCase()
+    ) {
+      return userData;
+    }
+  });
+
+  filterInputs();
+  inputFirstName.value = test;
+  filteredData.forEach((contactInfo) => {
+    console.log(contactInfo);
+    populateTable(contactInfo);
+  });
+  inputFirstName.focus();
+}
+
+function filterInputs() {
+  tr = document.createElement('tr');
+  contactList.appendChild(tr);
+  th = document.createElement('th');
+  th.scope = 'row';
+  th.innerHTML = '0';
+  tr.appendChild(th);
+
+  inputFirstName = document.createElement('input');
+  tdInputFirstName = document.createElement('td');
+  inputFirstName.placeholder = 'First Name';
+  tdInputFirstName.appendChild(inputFirstName);
+  tr.appendChild(tdInputFirstName);
+  inputFirstName.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputFirstName.addEventListener('keyup', function (e) {
+        search(data);
+      });
+    });
+  });
+
+  inputLastName = document.createElement('input');
+  tdInputLastName = document.createElement('td');
+  inputLastName.placeholder = 'Last Name';
+  tdInputLastName.appendChild(inputLastName);
+  tr.appendChild(tdInputLastName);
+  inputLastName.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputLastName.addEventListener('keyup', function (e) {
+        console.log(inputLastName.value);
+      });
+    });
+  });
+
+  inputAddress = document.createElement('input');
+  tdInputAddress = document.createElement('td');
+  inputAddress.placeholder = 'Address';
+  tdInputAddress.appendChild(inputAddress);
+  tr.appendChild(tdInputAddress);
+  inputAddress.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputAddress.addEventListener('keyup', function (e) {
+        console.log(inputAddress.value);
+      });
+    });
+  });
+
+  inputAddress2 = document.createElement('input');
+  tdInputAddress2 = document.createElement('td');
+  inputAddress2.placeholder = 'Address 2';
+  tdInputAddress2.appendChild(inputAddress2);
+  tr.appendChild(tdInputAddress2);
+  inputAddress2.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputAddress2.addEventListener('keyup', function (e) {
+        console.log(inputAddress2.value);
+      });
+    });
+  });
+
+  inputCity = document.createElement('input');
+  tdInputCity = document.createElement('td');
+  inputCity.placeholder = 'City';
+  tdInputCity.appendChild(inputCity);
+  tr.appendChild(tdInputCity);
+  inputCity.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputCity.addEventListener('keyup', function (e) {
+        console.log(inputCity.value);
+      });
+    });
+  });
+
+  inputState = document.createElement('select');
+  tdInputState = document.createElement('td');
+  tdInputState.appendChild(inputState);
+  tr.appendChild(tdInputState);
+  inputState.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputState.addEventListener('keyup', function (e) {
+        console.log(inputState.value);
+      });
+    });
+  });
+
+  inputZip = document.createElement('input');
+  tdInputZip = document.createElement('td');
+  inputZip.placeholder = 'Zip';
+  tdInputZip.appendChild(inputZip);
+  tr.appendChild(tdInputZip);
+  inputZip.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputZip.addEventListener('keyup', function (e) {
+        console.log(inputZip.value);
+      });
+    });
+  });
+
+  inputPhone = document.createElement('input');
+  tdInputPhone = document.createElement('td');
+  inputPhone.placeholder = 'Phone';
+  tdInputPhone.appendChild(inputPhone);
+  tr.appendChild(tdInputPhone);
+  inputPhone.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputPhone.addEventListener('keyup', function (e) {
+        console.log(inputPhone.value);
+      });
+    });
+  });
+
+  inputStatus = document.createElement('select');
+  tdInputStatus = document.createElement('td');
+  tdInputStatus.appendChild(inputStatus);
+  tr.appendChild(tdInputStatus);
+  inputStatus.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputStatus.addEventListener('keyup', function (e) {
+        console.log(inputStatus.value);
+      });
+    });
+  });
+
+  inputSource = document.createElement('select');
+  tdInputSource = document.createElement('td');
+  tdInputSource.appendChild(inputSource);
+  tr.appendChild(tdInputSource);
+  inputSource.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputSource.addEventListener('keyup', function (e) {
+        console.log(inputSource.value);
+      });
+    });
+  });
+
+  inputLastEditDate = document.createElement('input');
+  tdInputLastEditDate = document.createElement('td');
+  inputLastEditDate.placeholder = 'Last Edit Date';
+  tdInputLastEditDate.appendChild(inputLastEditDate);
+  tr.appendChild(tdInputLastEditDate);
+  inputLastEditDate.addEventListener('focusin', function (e) {
+    getJSON(ContactsURL).then((data) => {
+      inputLastEditDate.addEventListener('keyup', function (e) {
+        console.log('Just pressed Last Edit Date');
+      });
+    });
+  });
 }
 
 function populateTable(contactInfo) {
@@ -98,6 +285,7 @@ function populateTable(contactInfo) {
 
 getJSON(ContactsURL).then((data) => {
   //   console.log(data);
+  filterInputs();
   data.forEach((contactInfo) => {
     populateTable(contactInfo);
   });
